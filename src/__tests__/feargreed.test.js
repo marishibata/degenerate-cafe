86b6d57a4de91aaf14f936c42765b5670fad6b09
Jake <jake.w.miles@gmail.com>
2021-06-21 18:16:40 +0100

    feat: implement testing for axios to F&G endpoint

--- src/__tests__/feargreed.test.js
+++ src/__tests__/feargreed.test.js
@@ -1,2 +1,28 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 import FearGreed from '../Components/feargreed';
+
+jest.mock('axios', () => {
+  return {
+    default: () => {
+      return new Promise((resolve, reject) => {
+        resolve({
+          data: {data: [
+            {
+              value: 0,
+              value_classification: "Extreme Fear"
+            }
+          ]}
+        })
+      })
+    }
+  }
+})
+
+describe('fear and greed section', () => {
+  it('Successfully fetches F&G index', async () => {
+    const { findByText } = render(<FearGreed/>);
+    const result = await findByText('Extreme Fear');
+    expect(result).toBeTruthy();
+  })
+})
